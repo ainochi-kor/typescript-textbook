3675cec3e94a07ff9003952e4e3b109c09d09864
ainochi-kor <kthrkdals@gmail.com>
2025-04-27 00:44:42 +0900

    2.2 타입 추론을 적극 활용하자

--- ch02/2.1.ts
+++ ch02/2.1.ts
@@ -1,14 +1,16 @@
-const str: string = "hello world";
-const num: number = 123;
-const bool: boolean = false;
-const n: null = null;
-const u: undefined = undefined;
-const sym: symbol = Symbol("sym");
-const big: bigint = 10000000000n;
-const obj: object = { name: "John", age: 30 };
+() => {
+  const str: string = "hello world";
+  const num: number = 123;
+  const bool: boolean = false;
+  const n: null = null;
+  const u: undefined = undefined;
+  const sym: symbol = Symbol("sym");
+  const big: bigint = 10000000000n;
+  const obj: object = { name: "John", age: 30 };
 
-function plus(x: number, y: number): number {
-  return x + y;
-}
+  function plus(x: number, y: number): number {
+    return x + y;
+  }
 
-const minus = (x: number, y: number): number => x - y;
+  const minus = (x: number, y: number): number => x - y;
+};
---
+++ ch02/2.2.ts
@@ -0,0 +1,12 @@
+() => {
+  function plus(x: number, y: number): number {
+    return x + y;
+  }
+
+  const result2 = plus(1, 2);
+  // const result2: number
+
+  const str1: "hello" = "hello";
+  const str2: string = "hello";
+  const str3: {} = "hello";
+};
