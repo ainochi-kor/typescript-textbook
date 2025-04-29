c558de21f3a81a40440ff628dcb633075a7231ef
ainochi-kor <kthrkdals@gmail.com>
2025-04-29 23:31:51 +0900

    2.8 타입 별칭으로 타입에 이름을 붙이자

---
+++ ch02/2.8.ts
@@ -0,0 +1,19 @@
+() => {
+  type Person = {
+    name: string;
+    age: number;
+    married: boolean;
+  };
+
+  const person: Person = {
+    name: "zero",
+    age: 28,
+    married: false,
+  };
+
+  const person2: Person = {
+    name: "zero",
+    age: 32,
+    married: true,
+  };
+};
